20fa88f1ca7c653d88ced5396b18e2e6d3d55c0f
sandun <sandun@thesanmark.com>
2014-03-12 15:53:38 +0530

    Done 3425.

--- MerchantSharp/MerchantSharp/SanmarkSolutions/MerchantSharpApp/Model/Impl/SellingPriceManagerImpl.cs
+++ MerchantSharp/MerchantSharp/SanmarkSolutions/MerchantSharpApp/Model/Impl/SellingPriceManagerImpl.cs
@@ -212,6 +212,16 @@ namespace MerchantSharp.SanmarkSolutions.MerchantSharpApp.Model.Impl {
 
 		private void emergencyLoad() {
 			try {
+				System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+				dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+				dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 1);
+				dispatcherTimer.Start();
+			} catch ( Exception ) {
+			}
+		}
+
+		private void dispatcherTimer_Tick( object sender, EventArgs e ) {
+			try {
 				sellingPriceManager.DataTableUnitPrice = new DataTable();
 				sellingPriceManager.DataTableUnitPrice.Columns.Add("ID", typeof(int));
 				sellingPriceManager.DataTableUnitPrice.Columns.Add("Price", typeof(String));
@@ -230,6 +240,7 @@ namespace MerchantSharp.SanmarkSolutions.MerchantSharpApp.Model.Impl {
 					sellingPriceManager.grid_itemFinder.IsEnabled = false;
 					sellingPriceManager.textBox_selectedItemId.Text = sellingPriceManager.SelectedItemId.ToString();
 				}
+				( sender as System.Windows.Threading.DispatcherTimer ).Stop();
 			} catch ( Exception ) {
 			}
 		}
