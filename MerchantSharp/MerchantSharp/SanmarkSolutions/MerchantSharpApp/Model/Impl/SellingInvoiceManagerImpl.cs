6b3b10155ff27dd24b672ee30e259ceaa4b070f6
Sandun Dhanushka <sandun@thesanmark.com>
2014-07-28 08:15:03 +0530

    #16 Done.

--- MerchantSharp/MerchantSharp/SanmarkSolutions/MerchantSharpApp/Model/Impl/SellingInvoiceManagerImpl.cs
+++ MerchantSharp/MerchantSharp/SanmarkSolutions/MerchantSharpApp/Model/Impl/SellingInvoiceManagerImpl.cs
@@ -542,6 +542,10 @@ namespace MerchantSharp.SanmarkSolutions.MerchantSharpApp.Model.Impl {
         internal void selectItemById() {
             try {
                 Item item = itemManagerImpl.getItemById(Convert.ToInt32(addSellingInvoice.textBox_itemId_selectItem.Text));
+                if (addSellingInvoice.IsItemUpdateMode) {
+                    resetAddItemForm();
+                    addSellingInvoice.IsItemUpdateMode = false;
+                }
                 if (item != null) {
                     addSellingInvoice.SelectedItem = item;
                     populateAddItemForm();
